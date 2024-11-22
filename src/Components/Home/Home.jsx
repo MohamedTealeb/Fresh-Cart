35273b5f892783c7fd910f06a02ff0d33b7e0113
MohamedTealeb <mohamedtealeb088@gmail.com>
2024-11-22 22:40:17 +0200

    responsive design

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,26 +1,29 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Helmet } from "react-helmet"
-import Product from "../Product/Product"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import Product from "../Product/Product";
 
 export default function Home() {
-  const [products,setproduct]  = useState([])
-  useEffect(()=>{
-getproduct()
-  },[])
+  const [products, setproduct] = useState([]);
+  useEffect(() => {
+    getproduct();
+  }, []);
   async function getproduct() {
-    let {data}=await axios.get("https://ecommerce.routemisr.com/api/v1/products")
-    setproduct(data.data)
+    let { data } = await axios.get(
+      "https://ecommerce.routemisr.com/api/v1/products"
+    );
+    setproduct(data.data);
   }
-  return <>
-  <Helmet>
-    <title>Home</title>
-  </Helmet>
- <div className="grid grid-cols-4 gap-3">
-{products.map((product,id)=>{
-    return<Product product={product} key={id}/>
-}) }
- </div>
-  </>
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-3">
+        {products.map((product, id) => {
+          return <Product product={product} key={id} />;
+        })}
+      </div>
+    </>
+  );
 }
